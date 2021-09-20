c918ac800be70e4f6c880392cd9ea6153531ee72
YR1017fr <83820862+YR1017fr@users.noreply.github.com>
2021-09-20 18:12:56 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React,{Component} from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from'./components/Logo/Logo';
+import Rank from'./components/Rank/Rank';
+import ImageLinkForm from'./components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from'./components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import 'tachyons';
+
+const initialstate={
+  input:'',
+  imageUrl:'',
+  box:{},
+  route:'signin',  
+  isSignedin:false,
+  user:{
+    id:'',
+    name:'',
+    email:'',
+    entries:0,
+    joined:''
+  }
+}
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state=initialstate;
+  } 
+  
+  loadUser=(data)=>{   
+      this.setState({user:{
+        id:data.id,
+        name:data.name,
+        email:data.email,
+        entries:data.entries,
+        joined:data.joined
+      }})
+  }
+
+  calculateFaceLocation=(data)=>{
+      const face=data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image=document.getElementById('inputImage');
+      const width=Number(image.width);
+      const height=Number(image.height);
+      return{
+        leftCol:face.left_col*width,
+        topRow:face.top_row*height,
+        rightCol:(1-face.right_col)*width,
+        bottomRow:(1-face.bottom_row)*height
+      }
+  }
+  displayFaceBox=(box)=>{
+    this.setState({box:box})
+  }
+  onInputChange=(event)=>{
+    this.setState({input:event.target.value})
+  }
+  onImageSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('https://immense-ridge-71330.herokuapp.com/imageurl',{
+      method:'post',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify({
+        input:this.state.input
+      })
+    })
+      .then(response=>response.json())
+      .then(response =>{
+        if(response){
+          fetch('https://immense-ridge-71330.herokuapp.com/image',{
+            method:'put',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+                id:this.state.user.id
+            })
+          })
+          .then(response=>response.json())
+          .then(count=>{
+            this.setState(Object.assign(this.state.user,{entries:count}))
+          })
+          .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))})
+      .catch(err => console.log(err)
+    );
+  }
+onRouteChange=(route)=>{
+  if(route==='signin'){
+    this.setState(initialstate)
+  }else if(route==='home'){
+    this.setState({isSignedin:true})
+  }
+  this.setState({route:route});
+}
+onPressEnter=(event)=>{
+  if(event.key==='Enter'){
+      return this.onImageSubmit();
+  }
+}
+  
+
+
+
+
+  render(){
+    const {isSignedin, imageUrl,box,route} = this.state;
+    return(
+      <div className='App'>
+        <Navigation onRouteChange={this.onRouteChange} isSignedin={isSignedin}/>
+        {route ==='signin'
+          ? <Signin 
+              onRouteChange={this.onRouteChange}
+              loadUser={this.loadUser}
+            />
+          :(route==='home'
+            ?<div>
+                <Logo/>
+                <Rank
+                  name={this.state.user.name}
+                  entries={this.state.user.entries}
+                />          
+                <ImageLinkForm  onInputChange={this.onInputChange} onImageSubmit={this.onImageSubmit} onPressEnter={this.onPressEnter}/>
+                <FaceRecognition box={box} imageUrl={imageUrl}/>
+              </div>  
+            :<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
+        }        
+       
+      </div>
+    )
+  }
+}
+
+export default App;
